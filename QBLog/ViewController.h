07b52e86114e078a402aa21c48026dbebe5a2ebb
mahi888 <mahesh@stayntouch.com>
2015-08-17 13:54:36 +0530

    Initial Commit

---
+++ QBLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QBLog
+//
+//  Created by Mahesh M on 17/08/15.
+//  Copyright (c) 2015 qburst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QBLog/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QBLog
+//
+//  Created by Mahesh M on 17/08/15.
+//  Copyright (c) 2015 qburst. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
